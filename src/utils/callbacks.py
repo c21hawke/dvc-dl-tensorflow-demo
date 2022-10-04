f25705f6c8d01ff11080ff5c792abece538b2256
Chandra Reddy Tiyyagura <c21hawke@gmail.com>
2022-10-04 12:24:28 +0530

    callbacks added

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -17,4 +17,14 @@ def create_and_save_tensorboard_callback(callbacks_dir, tensorboard_log_dir):
     logging.info(f"Tensorboard callback is being saved at {tb_callback_flepath}")
 
 def create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir):
-    pass
+    checkpoint_file_path = os.path.join(checkpoint_dir, "ckpt_model.h5")
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath = checkpoint_file_path, 
+        save_best_only = True
+    )
+
+    ckpt_callback_flepath = os.path.join(callbacks_dir, "checkpint_cb.cb")
+    joblib.dump(checkpoint_callback, ckpt_callback_flepath)
+    logging.info(f"Tensorboard callback is being saved at {ckpt_callback_flepath}")
+
+
